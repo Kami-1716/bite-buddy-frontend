22a573c5e3236f32d5d7717d10e3bb9c46143b08
M Kamran <mkamran.4edu@gmail.com>
2024-04-27 10:27:06 +0500

    Update CuisineFilter component to include expand/collapse functionality

--- src/components/CuisineFilter.tsx
+++ src/components/CuisineFilter.tsx
@@ -1,6 +1,7 @@
 import { cuisineList } from "@/config/restaurant-options-config";
 import { Label } from "./ui/label";
-import { Check } from "lucide-react";
+import { Check, ChevronDown, ChevronUp } from "lucide-react";
+import { Button } from "./ui/button";
 
 type CuisineFilterProps = {
   onChange: (cuisine: string[]) => void;
@@ -42,32 +43,62 @@ const CuisineFilter = ({
         </div>
       </div>
       <div className="space-y-2 flex flex-col">
-        {cuisineList.map((cuisine) => {
-          const isChecked = selectedCuisines?.includes(cuisine);
-          return (
-            <div key={cuisine} className="flex items-center space-x-2">
-              <input
-                type="checkbox"
-                className="hidden"
-                value={cuisine}
-                id={`cuisine_${cuisine}`}
-                checked={isChecked}
-                onChange={handleChangeCuisine}
-              />
-              <Label
-                htmlFor={`cuisine_${cuisine}`}
-                className={`flex flex-1 items-center cursor-pointer font-semibold py-2 text-sm rounded-full px-3 ${
-                  isChecked
-                    ? "border border-green-600 text-green-600 bg-blue-100"
-                    : "border border-slate-300 bg-gray-200 text-gray-800"
-                }`}
-              >
-                {isChecked && <Check size={16} />} {cuisine}
-              </Label>
-            </div>
-          );
-        })}
+        {cuisineList
+          .slice(0, isExpanded ? cuisineList.length : 7)
+          .map((cuisine) => {
+            const isChecked = selectedCuisines?.includes(cuisine);
+            return (
+              <div key={cuisine} className="flex items-center space-x-2">
+                <input
+                  type="checkbox"
+                  className="hidden"
+                  value={cuisine}
+                  id={`cuisine_${cuisine}`}
+                  checked={isChecked}
+                  onChange={handleChangeCuisine}
+                />
+                <Label
+                  htmlFor={`cuisine_${cuisine}`}
+                  className={`flex flex-1 items-center cursor-pointer font-semibold py-2 text-sm rounded-full px-3 ${
+                    isChecked
+                      ? "border border-green-600 text-green-600 bg-blue-100"
+                      : "border border-slate-300 bg-gray-200 text-gray-800"
+                  }`}
+                >
+                  {isChecked && <Check size={16} />} {cuisine}
+                </Label>
+              </div>
+            );
+          })}
+
+        <Button variant="link" onClick={onExpandClick} className="mt-4 flex-1">
+          {isExpanded ? (
+            <span className="flex flex-row items-center">
+              View Less <ChevronUp size={16} />
+            </span>
+          ) : (
+            <span className="flex flex-row items-center">
+              View More <ChevronDown size={16} />
+            </span>
+          )}
+        </Button>
       </div>
+      {/* {isExpanded && (
+        <div
+          className="text-sm text-center font-semibold underline cursor-pointer mt-2"
+          onClick={onExpandClick}
+        >
+          Show Less
+        </div>
+      )}
+      {!isExpanded && (
+        <div
+          className="text-sm text-center font-semibold underline cursor-pointer mt-2"
+          onClick={onExpandClick}
+        >
+          Show More
+        </div>
+      )} */}
     </>
   );
 };
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -20,6 +20,8 @@ const SearchPage = () => {
     page: 1,
     selectedCuisines: [],
   });
+
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const { restaurants, isLoading } = useSearchRestaurants(SearchState, city);
 
   const setSelectedCuisines = (selectedCuisines: string[]) => {
@@ -74,6 +76,8 @@ const SearchPage = () => {
         <CuisineFilter
           onChange={setSelectedCuisines}
           selectedCuisines={SearchState.selectedCuisines}
+          isExpanded={isExpanded}
+          onExpandClick={() => setIsExpanded((prev) => !prev)}
         />
       </div>
       <div id="main-content" className="flex flex-col gap-5">
